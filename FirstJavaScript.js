eb34f186f98550462a0a08af639ad8b2080de135
MohdAatifSiddi <mekotecnolagy@gmail.com>
2025-02-19 11:51:40 +0530

    Create FirstJavaScript.js

---
+++ FirstJavaScript.js
@@ -0,0 +1,74 @@
+const now = 2025
+const ageAatif = now - 2007
+const ageAatis = now - 2008
+
+console.log(ageAatif)
+console.log(ageAatis)
+
+console.log(ageAati = 2 * 2, 4 / 3, 9 ** 3, 5 - 2, 5 + 2)
+/* Formula
+BMI = mass / height (height * height)
+
+*/
+
+/**
+let massMark = 78;
+let heightMark = 1.69;
+
+let massJohn = 92;
+let heightJohn = 1.95;
+
+
+let BMIJohn = massJohn / (heightJohn * heightJohn);
+
+let BMIMark = massMark / (heightMark * heightMark);
+
+console.log(BMIJohn);
+
+console.log(BMIMark);
+
+let markHigherBMI = massMark > massJohn;
+
+let JohnHigherBMI = massJohn > massMark;
+
+console.log(JohnHigherBMI);
+console.log(markHigherBMI);
+
+
+massMark = 95;
+heightMark = 1.88;
+
+massJohn = 85;
+heightJohn = 1.76;
+
+
+let is_Mark_Higher_BMI = massMark > massJohn;
+
+console.log(is_Mark_Higher_BMI);
+
+BMIJohn = massJohn / (heightJohn * heightJohn);
+
+BMIMark = massMark / (heightMark * heightMark);
+
+
+console.log(massMark);
+console.log(massJohn);
+
+**/
+
+
+const massMark = 78;
+const heightMark  = 1.69;
+
+const massJohn = 92;
+const heightJohn = 1.95;
+
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIJohn, BMIMark);
+
